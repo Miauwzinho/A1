8c41b4a484e27a9395e7b37fcd0b0a39d1e098df
Miauwzinho <161895867+Miauwzinho@users.noreply.github.com>
2024-03-01 19:40:50 -0300

    Add files via upload

---
+++ javascripti.js
@@ -0,0 +1,24 @@
+prompt("digite um número");
+var v1;
+v1 = Number (prompt ());
+console.log(v1);
+
+
+let a,b,c;
+a = prompt('primeiro numero');
+b = prompt('segundo numero');
+c = prompt('terceiro numero');
+
+let media = (a / 3) + (b / 3) + (c / 3);
+
+console.log(media)
+
+
+let x,y,z;
+
+x=10;
+y=20;
+z=30;
+console.log(x);
+console.log(y);
+console.log(z);
